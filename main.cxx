49576e32a13da705c7aee612a45fce44c027c717
Melchior FRANZ <mfranz@aon.at>
2010-04-27 13:41:11 +0200

    fix --reset

--- main.cxx
+++ main.cxx
@@ -371,13 +371,13 @@ int main(int argc, const char *argv[]) try
 
 		case RESET_OPTION:
 			log(INFO) << "resetting configuration to \"factory default\"" << endl;
-			if (!(dev.selected()->capabilities() & bu0836::CONFIG)) {
+			if (dev.selected()->capabilities() & bu0836::CONFIG) {
 				for (int i = 0; i < 8; i++) {
 					dev.selected()->set_invert(i, false);
 					dev.selected()->set_zoom(i, 0);
 				}
 			}
-			if (!(dev.selected()->capabilities() & bu0836::ENCODER)) {
+			if (dev.selected()->capabilities() & bu0836::ENCODER) {
 				for (int i = 0; i < 32; i += 2)
 					dev.selected()->set_encoder_mode(i, 0);
 				dev.selected()->set_pulse_width(6);
